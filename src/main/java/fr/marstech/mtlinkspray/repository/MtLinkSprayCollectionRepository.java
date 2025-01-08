e39368f093de8e980b7486a00b625ebe009d95ba
Alkaphreak <electron.libre@gmail.com>
2025-01-08 14:25:42 -0500

    Add MongoDB connection test on startup
    - Inject MtLinkSprayCollectionRepository in PostConstructConf
    - Rename MtLinkSprayCollection to MtLinkSprayCollectionItem
    - Add creationDate field to MtLinkSprayCollectionItem
    - Test MongoDB connection by saving and retrieving an item
    - Add MONGODB_URI environment variable in run configuration
    - Ignore .env file

--- src/main/java/fr/marstech/mtlinkspray/repository/MtLinkSprayCollectionRepository.java
+++ src/main/java/fr/marstech/mtlinkspray/repository/MtLinkSprayCollectionRepository.java
@@ -1,11 +1,13 @@
 package fr.marstech.mtlinkspray.repository;
 
-import fr.marstech.mtlinkspray.entity.MtLinkSprayCollection;
+import fr.marstech.mtlinkspray.entity.MtLinkSprayCollectionItem;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface MtLinkSprayCollectionRepository extends MongoRepository<MtLinkSprayCollection, String> {
+@Repository
+public interface MtLinkSprayCollectionRepository extends MongoRepository<MtLinkSprayCollectionItem, String> {
 
-    Optional<MtLinkSprayCollection> findById(String id);
+    Optional<MtLinkSprayCollectionItem> findById(String id);
 }
