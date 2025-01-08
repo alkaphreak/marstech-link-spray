58ba8b551b1356f8f3647b5c483755fb8f4087ee
Alkaphreak <electron.libre@gmail.com>
2025-01-08 14:25:42 -0500

    Add MongoDB connection test on startup
    - Inject MtLinkSprayCollectionRepository in PostConstructConf
    - Rename MtLinkSprayCollection to MtLinkSprayCollectionItem
    - Add creationDate field to MtLinkSprayCollectionItem
    - Test MongoDB connection by saving and retrieving an item
    - Add MONGODB_URI environment variable in run configuration
    - Ignore .env file

--- src/main/java/fr/marstech/mtlinkspray/conf/PostConstructConf.java
+++ src/main/java/fr/marstech/mtlinkspray/conf/PostConstructConf.java
@@ -1,5 +1,7 @@
 package fr.marstech.mtlinkspray.conf;
 
+import fr.marstech.mtlinkspray.entity.MtLinkSprayCollectionItem;
+import fr.marstech.mtlinkspray.repository.MtLinkSprayCollectionRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.java.Log;
 import org.springframework.context.annotation.Configuration;
@@ -7,36 +9,37 @@ import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
 import static java.text.MessageFormat.format;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @EnableAsync
 @Configuration
 @Log
 public class PostConstructConf {
 
+    final MtLinkSprayCollectionRepository mtLinkSprayCollectionRepository;
+
     private final Environment environment;
 
-    public PostConstructConf(
-            Environment environment
-    ) {
+    public PostConstructConf(Environment environment, MtLinkSprayCollectionRepository mtLinkSprayCollectionRepository) {
         this.environment = environment;
+        this.mtLinkSprayCollectionRepository = mtLinkSprayCollectionRepository;
     }
 
     @PostConstruct
     private void displayServerUrlInConsole() {
         CompletableFuture.runAsync(() -> {
             try {
-                TimeUnit.SECONDS.sleep(3);
+                SECONDS.sleep(3);
             } catch (InterruptedException e) {
                 // Do nothing because we don't care.
             }
 
-            log.info(format("Local server : http://localhost:{0}",
-                    environment.getProperty("server.port"))
-            );
+            log.info(format("Local server : http://localhost:{0}", environment.getProperty("server.port")));
         });
     }
 
@@ -44,10 +47,21 @@ public class PostConstructConf {
     private void testMongoDbConnection() {
         CompletableFuture.runAsync(() -> {
             try {
-                TimeUnit.SECONDS.sleep(10);
+                SECONDS.sleep(10);
 
                 String mongoDbUriEnvVar = environment.getProperty("spring.data.mongodb.uri");
                 log.info(format("MongoDB URI : {0}", mongoDbUriEnvVar));
+
+                String uuid = UUID.randomUUID().toString();
+                mtLinkSprayCollectionRepository.save(
+                        new MtLinkSprayCollectionItem(
+                                uuid,
+                                format("Description : {0}", uuid),
+                                LocalDateTime.now()
+                        )
+                );
+                assert mtLinkSprayCollectionRepository.findById(uuid).isPresent();
+                //mtLinkSprayCollectionRepository.deleteById(uuid);
 
             } catch (InterruptedException e) {
                 // Do nothing because we don't care.
--- src/main/java/fr/marstech/mtlinkspray/entity/MtLinkSprayCollectionItem.java
+++ src/main/java/fr/marstech/mtlinkspray/entity/MtLinkSprayCollectionItem.java
@@ -1,16 +1,23 @@
 package fr.marstech.mtlinkspray.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "mt-link-spray-collection")
-public class MtLinkSprayCollection {
+public class MtLinkSprayCollectionItem {
 
     @Id
     private String id;
 
     private String description;
 
-
+    private LocalDateTime creationDate;
 }
