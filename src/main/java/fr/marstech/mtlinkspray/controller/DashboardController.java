1b5e579b4428b83987ec84c8518557c833aef2a7
Alkaphreak <electron.libre@gmail.com>
2025-04-21 17:07:59 -0400

    Enhance dashboard functionality by adding conditional rendering for dashboard details and updating controller methods for improved data handling

--- src/main/java/fr/marstech/mtlinkspray/controller/DashboardController.java
+++ src/main/java/fr/marstech/mtlinkspray/controller/DashboardController.java
@@ -1,17 +1,19 @@
 package fr.marstech.mtlinkspray.controller;
 
+import fr.marstech.mtlinkspray.dto.DashboardDto;
 import fr.marstech.mtlinkspray.enums.ViewNameEnum;
 import fr.marstech.mtlinkspray.service.DashboardService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import static fr.marstech.mtlinkspray.enums.ViewNameEnum.DASHBOARD;
-import static fr.marstech.mtlinkspray.utils.NetworkUtils.getHeadersAsMap;
+import static java.text.MessageFormat.format;
 
 @Controller
 @Log
@@ -27,21 +29,27 @@ public class DashboardController implements ThymeleafViewControllerInterface {
 
     @GetMapping("/dashboard")
     public ModelAndView getView(HttpServletRequest httpServletRequest) {
-        return getModelAndView().addObject("headers", getHeadersAsMap(httpServletRequest));
+        return getModelAndView(httpServletRequest);
     }
 
     @PostMapping("/dashboard")
-    public ModelAndView postView(
-            @RequestParam(name = "input-dashboard-name") String inputDashboardName,
-            HttpServletRequest httpServletRequest
-    ) {
-        return getModelAndView()
-                .addObject("headers", getHeadersAsMap(httpServletRequest))
-                .addObject("dashboard", dashboardService.createDashboard(inputDashboardName));
+    public String postView(@RequestParam(name = "input-dashboard-name") String inputDashboardName) {
+        DashboardDto dto = dashboardService.createDashboard(inputDashboardName);
+        return getRedirectUrl(format("/dashboard/{0}", dto.getId()));
+    }
+
+    @GetMapping("/dashboard/{id}")
+    public ModelAndView getDashboardById(
+            @PathVariable(name = "id") String id, HttpServletRequest httpServletRequest) {
+        return getModelAndView(httpServletRequest).addObject("dashboard", dashboardService.getDashboard(id));
     }
 
     @Override
     public ModelAndView getModelAndView() {
         return getModelAndView(viewNameEnum);
+    }
+
+    public ModelAndView getModelAndView(HttpServletRequest httpServletRequest) {
+        return getModelAndView(viewNameEnum, httpServletRequest);
     }
 }
