10531ec0bab2aebf04bef3e901959875d128a2be
Alkaphreak <electron.libre@gmail.com>
2025-04-24 16:03:00 -0400

    release/0.0.3 (#22)
    * Update project version to 0.0.3 and add changelog and version update script
    
    * Update GitHub Actions workflow to support release branches and upgrade cache action

---
+++ dev-ops/mvn-new-version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script updates the version of a Maven project to the next version.
+# It is assumed that the script is run from the root directory of the project.
+VERSION="0.0.3"
+mvn versions:set -DnewVersion=$VERSION
