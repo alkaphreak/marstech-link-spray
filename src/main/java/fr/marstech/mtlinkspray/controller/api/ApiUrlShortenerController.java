08d9029d0fdc4d56ea4cc0eaaa1640545acdbe71
Alkaphreak <electron.libre@gmail.com>
2025-09-04 15:08:58 -0400

    MLS-74 Improve error handling for empty link shortening requests. (#32)
    feat: Add global exception handling and error page for URL shortener

--- src/main/java/fr/marstech/mtlinkspray/controller/api/ApiUrlShortenerController.java
+++ src/main/java/fr/marstech/mtlinkspray/controller/api/ApiUrlShortenerController.java
@@ -4,14 +4,17 @@ import fr.marstech.mtlinkspray.service.ShortenerService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import jakarta.validation.constraints.NotBlank;
 
 import static java.text.MessageFormat.format;
 
+@Validated
 @RestController
 public class ApiUrlShortenerController {
 
@@ -21,12 +24,29 @@ public class ApiUrlShortenerController {
     this.shortenerService = shortenerService;
   }
 
+  /**
+   * Generate a short link
+   *
+   * @param inputUrl the url input
+   * @param httpServletRequest th httpServletRequest
+   * @return the url shortened
+   */
   @GetMapping("/api/url-shortener/shorten")
   public String getShort(
-      @RequestParam(name = "url") String inputUrl, HttpServletRequest httpServletRequest) {
+      @RequestParam(name = "url") @NotBlank String inputUrl,
+      HttpServletRequest httpServletRequest) {
     return shortenerService.shorten(inputUrl, httpServletRequest);
   }
 
+  /**
+   * Forward a short link to the real link
+   *
+   * @param httpServletRequest the httpServletRequest
+   * @param shortUrlUid the short code of the url
+   * @param model the model of the view
+   * @return a {@link ModelAndView} with forward to the target url
+   * @throws ChangeSetPersister.NotFoundException in case the db did not find
+   */
   @GetMapping("/{shortUrlUid}")
   public ModelAndView getTarget(
       HttpServletRequest httpServletRequest,
