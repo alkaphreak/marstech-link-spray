4e665a9d01e3ae3c1a40a828311311d8d00f994a
Alkaphreak <electron.libre@gmail.com>
2025-10-07 09:59:37 -0400

    feat: change ReportAbuseService to a functional interface for improved usability

--- src/main/kotlin/fr/marstech/mtlinkspray/service/ReportAbuseService.kt
+++ src/main/kotlin/fr/marstech/mtlinkspray/service/ReportAbuseService.kt
@@ -2,7 +2,7 @@ package fr.marstech.mtlinkspray.service
 
 import jakarta.servlet.http.HttpServletRequest
 
-interface ReportAbuseService {
+fun interface ReportAbuseService {
     fun reportAbuse(
         inputAbuseDecsription: String,
         httpServletRequest: HttpServletRequest
